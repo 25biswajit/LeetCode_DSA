cd601bcef099d7613d09b1f8c21bb86980455a92
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-06-07 08:25:42 +0530

    Time: 4 ms (97.44%), Space: 45.2 MB (80%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -0,0 +1,41 @@
+class Solution {
+    int[][] dirs = {{-1,0}, {1,0}, {0,-1},{0,1}};
+    public List<List<Integer>> pacificAtlantic(int[][] h) {
+        List<List<Integer>> list = new ArrayList<>();
+        int row = h.length;
+        int col = h[0].length;
+        boolean[][] pacific = new boolean[row][col];
+        boolean[][] atlantic = new boolean[row][col];
+
+        // DFS
+        //Row wise 
+        for(int i = 0; i < row; i++){
+            dfs(h, i, 0, Integer.MIN_VALUE, row, col, pacific);
+            dfs(h, i, col-1, Integer.MIN_VALUE, row, col, atlantic);
+        }
+        //col wise
+        for(int i = 0; i < col; i++){
+            dfs(h, 0, i, Integer.MIN_VALUE, row, col, pacific);
+            dfs(h, row-1, i, Integer.MIN_VALUE, row, col, atlantic);
+        }
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(pacific[i][j] && atlantic[i][j]){
+                    list.add(Arrays.asList(i,j));
+                }
+            }
+        }
+
+        return list;
+    }
+
+    private void dfs(int[][] h, int i, int j, int prev, int row, int col, boolean[][] ocean){
+        if(i < 0 || i >= row || j < 0 || j >= col) return;
+        if(h[i][j] < prev || ocean[i][j]) return;
+        ocean[i][j] = true;
+        for(int d[] : dirs){
+            dfs(h, i+d[0], j+d[1], h[i][j], row, col, ocean);
+        }
+    }
+}
