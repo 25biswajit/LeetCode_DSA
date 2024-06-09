32ab0668c9ac466ea9e37a7feb13e9c655e7fca5
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-06-09 09:56:26 +0530

    Update 0188-best-time-to-buy-and-sell-stock-iv.java

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,22 +1,30 @@
 class Solution {
-    public int maxProfit(int k, int[] prices) {
+    public int maxProfit(int p, int[] prices) {
         int n = prices.length;
-        int dp[][] = new int[n][2];
-        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
-        return maxProfit(k, 0, n, 0, prices, dp);
+        int dp[][][] = new int[n][p+1][2];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < p+1; j++){
+                for(int k = 0; k < 2; k++){
+                    dp[i][j][k] = -1;
+                }
+            }   
+        }
+
+        return maxProfit(p, 0, n, 0, prices, dp);
     }
 
-    int maxProfit(int k, int i, int n, int flag, int[] a, int[][] dp){
-        if(i >= n || k == 0) return 0;
-        if(dp[i][flag] != -1) return dp[i][flag];
+    int maxProfit(int k, int i, int n, int flag, int[] a, int[][][] dp) {
+        if (i == n || k == 0) return 0;
+        if (dp[i][k][flag] != -1) return dp[i][k][flag];
         int noAction = 0, take = 0;
-        if(flag == 0){//buy
-            take = - a[i] + maxProfit(k, i+1, n, 1, a, dp);
+        if (flag == 0) {// buy
+            take = -a[i] + maxProfit(k, i + 1, n, 1, a, dp);
+        } 
+        else if (flag == 1) {// sell
+            take = a[i] + maxProfit(k - 1, i + 1, n, 0, a, dp);
         }
-        else if(flag == 1){//sell
-            take = a[i] + maxProfit(k-1, i+1, n, 0, a, dp);
-        }
-        noAction = maxProfit(k, i+1, n, flag, a, dp);
-        return dp[i][flag] = Integer.max(noAction, take);
+        noAction = maxProfit(k, i + 1, n, flag, a, dp);
+        return dp[i][k][flag] = Integer.max(noAction, take);
     }
 }
