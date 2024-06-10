326c1e94428790394e8699b531d24a9ba673ca91
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-06-10 19:55:56 +0530

    Update 0181-employees-earning-more-than-their-managers.sql
    Optimised

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,6 @@
 /* Write your PL/SQL query statement below */
 
-select T.emp_name Employee from 
+/*select T.emp_name Employee from 
 (
 select e1.id emp_id, e1.salary emp_sal, e1.name emp_name,
 e2.id mgr_id, e2.salary mgr_sal 
@@ -11,3 +11,12 @@ Employee e2
 on e1.managerId = e2.id
 ) T
 where T.emp_sal > T.mgr_sal
+*/
+
+select e1.name Employee
+from 
+Employee e1
+left join
+Employee e2
+on e1.managerId = e2.id
+where e1.salary > e2.salary
