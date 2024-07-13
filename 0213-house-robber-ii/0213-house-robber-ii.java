db366bea9b34d800acbe3c22ef71ca915e35da1d
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-07-13 18:42:25 +0530

    Time: 0 ms (100%), Space: 40.9 MB (37.37%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,22 @@
 class Solution {
-    int[] dp = null;
     public int rob(int[] a) {
         int n = a.length;
         if(n == 1) return a[0];
-        dp = new int[n];
-        Arrays.fill(dp,-1);
-        int ans = solve(a,0,n-2,0);
-        Arrays.fill(dp,-1);
-        return Integer.max(ans, solve(a,1,n-1,1));
+        int dp[] = new  int[n];
+        Arrays.fill(dp, -1);
+        int ans1 = rob(0, n-2, a, dp);
+        Arrays.fill(dp, -1);
+        int ans2 = rob(1, n-1, a, dp);
+        return Integer.max ( ans1 , ans2 );
     }
 
-    private int solve(int[] a, int s, int e, int i){
-        if(i > e) return 0;
+    private int rob(int i, int n, int[] a, int[] dp){
+        if(i > n) return 0;
         if(dp[i] != -1) return dp[i];
-
-        int noaction = solve(a,s,e,i+1);
-        int action = solve(a,s,e,i+2) + a[i];
-        int result = Integer.max(noaction, action);
-        return dp[i] = result;
+        int noAction = rob(i+1, n, a, dp);
+        int take = a[i] + rob(i+2, n, a, dp);
+        return dp[i] = Integer.max(noAction, take);
     }
+
+    
 }
