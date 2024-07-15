d08dee241a778db204164bcb89543f986c42846e
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-07-15 09:59:17 +0530

    Time: 4 ms (49.04%), Space: 42.6 MB (16.9%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,60 +1,64 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        int minlevel = Integer.MAX_VALUE , maxlevel = Integer.MIN_VALUE;
-        //PriorityQueue<Pair> q = new PriorityQueue<>((a,b)-> a.row==b.row ? a.col - b.col  : a.row - b.row);
-        Queue<Pair> q = new LinkedList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) return ans;
-        HashMap<Integer, List<Pair>> map = new HashMap<>();
-        q.add(new Pair(root, 0, 0));
+        {
+            int minL = Integer.MAX_VALUE, maxL = Integer.MIN_VALUE;
+            int row = 0;
+            Map<Integer, List<Pair>> map = new HashMap<>();
+            Queue<Pair> queue = new LinkedList<>();
+            queue.add(new Pair(root, row, 0));
 
-        while(!q.isEmpty()){
-            Pair top = q.poll();
-            addToMap(top, map);
-            minlevel = Integer.min(minlevel, top.col);
-            maxlevel = Integer.max(maxlevel, top.col);
-            if(top.node.left != null){ q.add( new Pair(top.node.left, top.row + 1, top.col - 1));}
-            if(top.node.right != null){ q.add( new Pair(top.node.right, top.row + 1, top.col + 1));}
-        }
+            while (!queue.isEmpty()) {
+                int n = queue.size();
+                for (int i = 0; i < n; i++) {
+                    Pair top = queue.poll();
+                    minL = Integer.min(minL, top.col);
+                    maxL = Integer.max(maxL, top.col);
 
-        for(int i = minlevel ; i <= maxlevel; i++){
-            List<Pair> list = map.get(i);
-            Collections.sort(list, (a,b)-> a.col==b.col && a.row==b.row ? a.node.val - b.node.val  : a.row - b.row);
-            List<Integer> res = new ArrayList<Integer>();
-            for(Pair p : list){
-                res.add(p.node.val);
+                    map.putIfAbsent(top.col, new ArrayList<>());
+                    map.get(top.col).add(top);
+
+                    if (top.node.left != null) {
+                        queue.add(new Pair(top.node.left, top.row + 1, top.col - 1));
+                    }
+                    if (top.node.right != null) {
+                        queue.add(new Pair(top.node.right, top.row + 1, top.col + 1));
+                    }
+                }
             }
-            ans.add(res);
-        }
-        
-        return ans;
-    }
 
-    private void addToMap(Pair top, HashMap<Integer, List<Pair>> map){
-       map.computeIfAbsent(top.col, k -> new ArrayList<>()).add(top);
+            List<List<Integer>> result = new ArrayList<>();
+            while (minL <= maxL) {
+                List<Pair> list = map.get(minL);
+                Collections.sort(list, (a,b)-> a.col==b.col && a.row==b.row ? a.node.val - b.node.val  : a.row - b.row);
+                result.add(list.stream().map(p -> p.node.val).collect(Collectors.toList()));
+                minL++;
+            }
+
+            return result;
+        }
     }
 }
-
 class Pair{
     TreeNode node;
     int row;
     int col;
-    Pair(TreeNode node, int row, int col){
+    
+    public Pair(TreeNode node, int row, int col){
         this.node = node;
         this.row = row;
         this.col = col;
