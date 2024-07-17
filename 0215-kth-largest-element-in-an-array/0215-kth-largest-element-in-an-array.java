3f95c99da9e963f6584c6d2888345e68b0186073
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-07-17 06:58:31 +0530

    Time: 58 ms (54.24%), Space: 61.5 MB (12.84%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,40 +1,13 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int s = 0, e = nums.length - 1, index = -1;
-        while(s <= e){
-            index = quickSelect(nums, s, e);
-            if(index == k-1) break;
-            else if(index > k-1) e = index - 1;
-            else s = index + 1;
-        }
-        return nums[index];
-    }
-
-    public int quickSelect(int[] a, int s, int e){
-        int p = s;
-        s++;
-        while(s <= e){
-            if(a[s] < a[p] && a[e] > a[p]){
-                swap(a, s, e);
-                s++;
-                e--;
-            }
-            if(a[s] >= a[p]){
-                s++;
-            }
-            if(a[e] <= a[p]){
-                e--;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->a-b);
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            pq.add(nums[i]);
+            if(pq.size() > k){
+                pq.poll();
             }
         }
-        swap(a, p, e);
-        return e;
+        return pq.isEmpty() ? -1 : pq.poll();
     }
-
-    public void swap(int[] a, int i, int j){
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-
-
 }
