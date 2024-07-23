8e15906a42774c01f990153c3e1a4d7d04f8f34e
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-07-23 20:16:55 +0530

    Time: 1 ms (98.89%), Space: 45.1 MB (12.79%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,32 +1,32 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int n = intervals.length;
-        List<int[]> list = new ArrayList<>();
+       int n = intervals.length;
+       List<int[]> list = new ArrayList<>();
+       for(int i =0; i < n;i++){
+            int[] temp = intervals[i];
 
-        for(int i = 0; i < n; i++){
-            int[] curr = intervals[i];
-            //newInt after
-            if(newInterval[0] > curr[1]){
-                list.add(curr);
-            }
-            //newInt before
-            else if(newInterval[1] < curr[0]){
+            //case1 - new < temp
+            if(newInterval[1] < temp[0]){
                 list.add(newInterval);
-                newInterval = curr;
+                newInterval = temp;
             }
-            //overlap
+            //case2 - new > temp
+            else if(newInterval[0] > temp[1]){
+                list.add(temp);
+            }
+            //case3 - overlapping
             else{
-                newInterval[0] = Integer.min(newInterval[0], curr[0]);
-                newInterval[1] = Integer.max(newInterval[1], curr[1]);
+                newInterval[0] = Integer.min(newInterval[0], temp[0]);
+                newInterval[1] = Integer.max(newInterval[1], temp[1]);
             }
-        }
-        list.add(newInterval);
+       } 
+       list.add(newInterval);
 
-        int[][] res = new int[list.size()][2];
-        int i = 0;
-        for(int[] p : list){
-            res[i++] = p;
-        }
-        return res;
+       n = list.size(); 
+       int[][] result = new int[n][];
+       for(int i = 0; i < n;i++){
+            result[i] = list.get(i);
+       }
+       return result;
     }
 }
