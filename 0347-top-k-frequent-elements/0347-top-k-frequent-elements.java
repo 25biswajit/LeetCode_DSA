71d2f0c33ba7b709bb4a6d5d3a8c1c2b1f8072d0
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-08-05 18:03:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,16 +2,30 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int[] ans = new int[k];
         Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1]-b[1]);
+
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+            pq.add(new int[]{ i, map.get(i)});
+            if(pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        return pq.stream().mapToInt(i -> i[0]).toArray();
+    }
+
+    public int[] topKFrequent_old(int[] nums, int k) {
+        int[] ans = new int[k];
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i : nums){
             map.put(i, map.getOrDefault(i,0)+1);
         }
 
-        return map
-        .entrySet()
-        .stream()
-        .sorted((a,b)->b.getValue() - a.getValue())
-        .mapToInt(i -> i.getKey())
-        .limit(k)
-        .toArray();
+        return map.entrySet().stream()
+                            .sorted((a,b)->b.getValue() - a.getValue())
+                            .mapToInt(i -> i.getKey())
+                            .limit(k)
+                            .toArray();
     }
 }
