984ff3cf2bd72695e448b094287196d5a7fffe13
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-08-05 19:03:46 +0530

    Time: 17 ms (17.45%), Space: 48 MB (78.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,5 +1,37 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        int maxFreq = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+            maxFreq = Integer.max( maxFreq, map.get(i));
+        }
+        
+        List<Integer>[] buckets = new ArrayList[maxFreq + 1];
+        for(Integer key : map.keySet()){
+            int index = map.get(key);
+            if(buckets[ index ] == null){
+                buckets[ index ] = new ArrayList<>();
+            }
+            buckets[ index ].add(key);
+        }
+
+        int[] ans = new int[k];
+        int i = 0;
+
+        for(int j = maxFreq; j >= 0 && i < k; j--){
+            if(buckets[ j ] != null){
+                for(int num : buckets[ j ]){
+                    ans[i++] = num;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+
+    public int[] topKFrequent_pq(int[] nums, int k) {
         int[] ans = new int[k];
         Map<Integer, Integer> map = new HashMap<>();
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1]-b[1]);
