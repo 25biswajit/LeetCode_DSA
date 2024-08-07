51052f947b63680cbdfd1fbf533cf7a3ef604997
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-08-07 16:56:37 +0530

    Time: 1 ms (97.79%), Space: 44.7 MB (82.83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,15 @@
 class Solution {
     public int jump(int[] a) {
-        int max = 0, n = a.length, jump = 0;
-        int l = 0, r = 0;
+        int left = 0, right = 0, maxReach = 0, n = a.length, jump = 0;
 
-        while(r < n-1){
-            for(int i = l; i <= r; i++){
-                int reach = i + a[i];
-                max = Integer.max(max, reach);
+        while(right < n-1){
+            for(int i = left; i <= right; i++){
+                maxReach = Integer.max(maxReach, a[i] + i);
             }
-            l = r + 1;
-            r = max;
+            left = right + 1;
+            right = maxReach;
             jump++;
-        }    
+        }
         return jump;
     }
 }
