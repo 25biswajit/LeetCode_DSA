bdbf774b013000f2d88fa61a33aac85b228bbbd5
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-08-08 11:19:07 +0530

    Time: 35 ms (18.97%), Space: 45.5 MB (12.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,25 @@
 class Solution {
+    int[][] dp = null;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][] = new int[n+1][amount+1];
-        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
-        int result = coinChange(0, coins, amount, n, dp);
-        return result == Integer.MAX_VALUE - 1 ? -1 : result;
+        if(amount == 0) return 0; 
+        dp = new int[n][amount+1];
+        Arrays.stream(dp).forEach(d -> Arrays.fill(d, -1));
+        int result = solve(coins, n, amount, 0);
+        return result >= Integer.MAX_VALUE-1 ? -1 : result;
     }
 
-    private int coinChange(int i, int a[], int k, int n, int dp[][]){
-        if(k == 0) return 0;
-        if(i >= n || k < 0) return Integer.MAX_VALUE-1;
-        if(dp[i][k]!=-1) return dp[i][k];
-        int take = Integer.MAX_VALUE;
-        int noAction = coinChange(i+1, a, k, n, dp);
-        if(k >= a[i]){
-            take = 1 + coinChange(i, a, k-a[i], n, dp);
+    private int solve(int[] coins, int n, int amount, int i){
+        if(amount == 0) return 0;
+        if(i >= n) return Integer.MAX_VALUE-1;
+        if(dp[i][amount] != -1) return dp[i][amount];
+
+        int take = Integer.MAX_VALUE-1;
+        if(coins[i] <= amount){
+            take = solve(coins, n, amount - coins[i], i) + 1;
         }
-        return dp[i][k] = Integer.min(take, noAction);
+        int leave = solve(coins, n, amount, i+1);
+        int result = Integer.min(take, leave); 
+        return dp[i][amount] = result;
     }
 }
