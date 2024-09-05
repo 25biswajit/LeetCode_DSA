33fc3b044ab63fbb0a60347c8b5e77601d4595ad
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-09-05 12:10:33 +0530

    Time: 0 ms (100%), Space: 41.2 MB (19.27%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,20 +2,19 @@ class Solution {
     int[] dp = null;
     public int rob(int[] a) {
         int n = a.length;
-        if(n==1) return a[0];
-        dp = new int[n+1];
-        Arrays.fill(dp , -1);
-        int ans1 = rob(a, 0, n-1);
-        Arrays.fill(dp , -1);
-        int ans2 = rob(a, 1, n);
-        return Math.max(ans1, ans2);
+        if(n == 1) return a[0];
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        int ans = money(a, 0, n-2);
+        Arrays.fill(dp, -1);
+        return Integer.max(ans, money(a, 1, n-1));
     }
 
-    int rob(int[] a, int i, int n){
-        if(i >= n) return 0;
+    int money(int[] a, int i, int e){
+        if(i > e) return 0;
         if(dp[i]!= -1) return dp[i];
-        int take = a[i] + rob(a, i+2, n);
-        int leave = rob(a, i+1, n);
-        return dp[i] = Math.max(take, leave);
+        int take = a[i] + money(a, i+2, e);
+        int leave = money(a, i+1, e);
+        return dp[i] = Integer.max(take, leave);
     }
 }
