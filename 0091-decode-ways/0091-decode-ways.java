b050d6680b8e1f934f4460d111bdf9f0281a2dd1
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-09-06 11:14:18 +0530

    Time: 2 ms (20.75%), Space: 41.8 MB (39.84%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -2,29 +2,29 @@ class Solution {
     Map<String, Integer> map = null;
     public int numDecodings(String s) {
         map = new HashMap<>();
-        return ways(s);
+        return decode(s);
     }
 
-    private int ways(String s){
+    int decode(String s){
         int n = s.length();
         if(n == 0) return 1;
-        if(s.charAt(0)=='0') return 0;
         if(map.containsKey(s)) return map.get(s);
-
-        int result = 0;
-        String s1 = s.substring(0,1);
-        result += ways(s.substring(1));
-
-        String s2 = null;
-        if(n > 1){
-            s2 = s.substring(0,2);
-            int num = Integer.parseInt(s2);
-            if(num <= 26){
-                result += ways(s.substring(2));
-            }
+        if(n > 0 && s.charAt(0)=='0') return 0;
+        //if(n == 1) return 1;
+        //int num = Integer.parseInt(s);
+        //if(n == 2 && num >=11 && num<= 26) return 2;
+        
+        int ways = decode( s.substring(1) );
+        if(n >= 2 && isValid(s.substring(0,2))){
+            ways += decode(s.substring(2));
         }
-
-        map.put(s, result);
-        return result;
+        map.put(s, ways);
+        return ways;
     }
+    
+    boolean isValid(String s){
+        int num = Integer.parseInt(s);
+        return num >= 1 && num <= 26;
+    }
+
 }
