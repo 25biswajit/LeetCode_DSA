2a6969ecb00ae3ebd2e395b38ff0e8f8ff32d538
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-09-06 20:19:58 +0530

    Time: 5 ms (6.61%), Space: 48.6 MB (6.65%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,20 +1,22 @@
 class Solution {
-    public int maxProfit(int[] a) {
-        //flag buy = 1, sell 0
-        int n = a.length;
-        int[][] dp = new int[n][2];
-        Arrays.stream(dp).forEach( sa -> Arrays.fill(sa,-1));
-        return trade(a, 0, 1, dp);
+    int[][] dp = null;
+    public int maxProfit(int[] arr) {
+        int n = arr.length;
+        dp = new int[n][2];
+        Arrays.stream(dp).forEach(a -> Arrays.fill(a,-1));
+        return profit(arr, n, 0, 0); //buy 0 sell 1
     }
-
-    private int trade(int[] a, int i, int flag, int[][] dp){
-        int n = a.length;
+ 
+    int profit(int[] a, int n, int i, int f){
         if(i >= n) return 0;
-        if(dp[i][flag] != -1) return dp[i][flag];
-        int noaction = trade(a, i+1, flag, dp);
-        int action = flag == 1 ? trade(a, i+1, 0, dp)-a[i] : trade(a, i+1, 1, dp)+a[i];
-        int result = Integer.max( noaction, action );
-
-        return dp[i][flag] = result;
+        if(dp[i][f] != -1) return dp[i][f];
+        int action = 0;
+        int noaction = profit(a, n, i+1, f);
+        if(f == 1){ // sell
+            action = profit(a, n, i+1, 0) + a[i];
+        }else{ // buy
+            action = profit(a, n, i+1, 1) - a[i];
+        }
+        return dp[i][f] = Integer.max(noaction, action);
     }
 }
