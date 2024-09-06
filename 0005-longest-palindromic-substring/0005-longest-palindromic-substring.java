a9a661df64925a9135e6f366c57bea7a65c55b32
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-09-06 23:26:09 +0530

    Time: 225 ms (28.1%), Space: 45.5 MB (22.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,28 @@
 class Solution {
     public String longestPalindrome(String s) {
+        String result = "", temp = "", odd = "", even = "";
         int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int maxLength = 1;  // Length of the longest palindrome found
-        int start = 0;      // Start index of the longest palindrome
-        
-        for (int len = 1; len <= n; len++) {
-            for (int i = 0; i + len <= n; i++) {
-                int j = i + len - 1;
-                boolean flag = s.charAt(i) == s.charAt(j);
-                
-                // For substrings of length 1 and 2
-                if (len <= 2) {
-                    dp[i][j] = flag;
-                } else {
-                    dp[i][j] = dp[i + 1][j - 1] && flag;
-                }
-                
-                // If it's a palindrome and longer than the previous found
-                if (dp[i][j] && len > maxLength) {
-                    start = i;
-                    maxLength = len;
-                }
-            }
+        for(int i = 0; i < n; i++){
+            even = expand(s, i, i+1);
+            odd = expand(s, i, i);
+            temp = even.length() > odd.length() ? even : odd;
+            result = result.length() > temp.length() ? result : temp;
         }
-        
-        return s.substring(start, start + maxLength);
+        return result;
+    }
+
+    public String expand(String s, int i, int j){
+        int n = s.length();
+        String ans = "";
+        while(i>=0 && j<n && s.charAt(i)==s.charAt(j)){
+            if(i==j){
+                ans = s.charAt(i) + ans; 
+            }else{
+                ans = s.charAt(i) + ans + s.charAt(j);
+            }
+            i--;
+            j++;
+        }
+        return ans;
     }
 }
