a49f833efa005529736583032924432a749f603a
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-09-08 14:59:05 +0530

    Time: 31 ms (56.96%), Space: 51.3 MB (68.08%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,51 +1,45 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int target = 0;
-        List<List<Integer>> list = new ArrayList<>();
-        int n = nums.length, j, k;
-        for(int i = 0; i <=n-3; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            j = i+1;
-            k = n-1;
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == target) {
-                    list.add(Arrays.asList(nums[i] , nums[j] , nums[k]));
-                
-                    while(j < k && nums[j] == nums[j+1]) j++;
-                    while(j < k && nums[k] == nums[k-1]) k--;
-
-                    j++;
-                    k--;
+    public List<List<Integer>> threeSum(int[] a) {
+        Arrays.sort(a);
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = a.length;
+        for(int i = 0; i < n-2; i++){
+            if(i > 0 && a[i] == a[i-1]) continue;
+            int l = i+1, h = n-1;
+            while(l < h){
+                int sum = a[i] + a[l] + a[h];
+                if(sum == 0){
+                    List<Integer> list = Arrays.asList(a[i], a[l], a[h]);
+                    ans.add(list);
+                    while(l < h && a[l] == a[l+1]) l++;
+                    while(l < h && a[h] == a[h-1]) h--;
+                    l++;
+                    h--;
                 }
-                else if(sum > target){
-                    k--;
+                else if(sum < 0){
+                    l++;
                 }
-                // sum < target
-                else{
-                    j++;
+                else{//sum > 0
+                    h--;
                 }
             }
         }
-        return list;
+        return ans;
     }
-    
-    public List<List<Integer>> threeSum_(int[] nums) {
-        int target = 0;
-        List<List<Integer>> list = new ArrayList<>();
-        Set<Integer> set = null;
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            set = new HashSet<>();
-            for(int j = i+1; j < n; j++){
-                int rest = target - (nums[i] + nums[j]);
-                if(set.contains(rest)){
-                    list.add(Arrays.asList(nums[i] , nums[j] , rest));
+
+    public List<List<Integer>> threeSum_1(int[] a) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = a.length;
+        for(int i = 0; i < n-2; i++){
+            for(int j = i+1; j < n-1; j++){
+                for(int k = j+1; k < n; k++){
+                    if(a[i]+a[j]+a[k] == 0){
+                        List<Integer> list = Arrays.asList(a[i], a[j], a[k]);
+                        ans.add(list);
+                    }
                 }
-                set.add(nums[j]);
             }
         }
-        return list;
+        return ans;
     }
 }
