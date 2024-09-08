3d6adb9a025c347527986b3ecc4ad15793e34cda
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-09-08 13:07:01 +0530

    Time: 46 ms (19.75%), Space: 61.3 MB (26.66%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,27 +1,20 @@
 class Solution {
     public int[] maxSlidingWindow(int[] a, int k) {
         List<Integer> list = new ArrayList<>();
-        // add to back , delete from front
         Deque<Integer> dq = new ArrayDeque<>();
         int n = a.length;
-        for(int i = 0; i < n;i++){
-            // if window is over then delete from Front
+        for(int i = 0; i < n; i++){
             if(!dq.isEmpty() && i - dq.peekFirst() + 1 > k){
-                dq.pollFirst();
+                dq.removeFirst();
             }
-
-            // if dp.front i.e. max < current, delete max
-            while(!dq.isEmpty() && a [ dq.peekLast() ]  < a[i]){
-                dq.pollLast();
+            while(!dq.isEmpty() && a[ dq.peekLast() ] < a[i]){
+                dq.removeLast();
             }
-
             dq.addLast(i);
-
-            if(i >= k-1 && !dq.isEmpty()){
-                list.add(a[ dq.peekFirst() ]);
+            if(!dq.isEmpty() && i >= k-1){
+                list.add ( a[ dq.peekFirst()] );
             }
         }
-
-        return list.stream().mapToInt(i -> i).toArray();
+        return list.stream().mapToInt(i->i).toArray();
     }
 }
