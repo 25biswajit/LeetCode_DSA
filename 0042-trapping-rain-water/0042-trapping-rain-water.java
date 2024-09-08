9ba9bdc6e4f7d163a328335ffb3607e9bc2c945c
BISWAJIT SARKAR <25biswajit@gmail.com>
2024-09-08 19:11:26 +0530

    Update 0042-trapping-rain-water.java

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,5 +1,21 @@
 class Solution {
-    public int trap(int[] height) {
+    public int trap(int[] a) {
+        int ans = 0;
+        int n = a.length;
+        int[] lMax = new int[n];
+        lMax[0] = a[0];
+        for(int i = 1; i < n; i++){
+            lMax[i] = Integer.max(lMax[i-1], a[i]);
+        }
+        int rMax = Integer.MIN_VALUE;
+        for(int i = n-1; i >= 0; i--){
+            rMax = Integer.max(rMax, a[i]);
+            ans += Integer.min(lMax[i], rMax)-a[i];
+        }
+        return ans;
+    }
+    
+    public int trap_(int[] height) {
         int n = height.length;
         int[] prefix = new int[n];
         int[] suffix = new int[n];
